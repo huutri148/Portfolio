9593b37cb6fb6b09104e0ca270128157bde06513
huutri148 <huutri148@gmail.com>
2022-12-24 02:14:12 +0700

    feat: add storybook

---
+++ .storybook/main.js
@@ -0,0 +1,29 @@
+const path = require("path");
+
+module.exports = {
+  stories: ["../src/**/*.stories.mdx", "../src/**/*.stories.@(js|jsx|ts|tsx)"],
+  addons: [
+    "@storybook/addon-links",
+    "@storybook/addon-essentials",
+    "@storybook/addon-interactions",
+    {
+      name: "storybook-addon-next",
+      options: {
+        nextConfigPath: path.resolve(__dirname, "../next.config.js")
+      }
+    }
+  ],
+  framework: "@storybook/react",
+  core: {
+    builder: "@storybook/builder-webpack5"
+  },
+  staticDirs: ["../public"],
+  webpackFinal: async (config, { configType }) => {
+    config.resolve.alias = {
+      ...config.resolve.alias,
+      "@": path.resolve(__dirname, "../src/")
+    };
+
+    return config;
+  }
+};
