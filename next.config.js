c0cb5790809c253778008ff489bf0022aa64f0e7
huutri148 <huutri148@gmail.com>
2022-12-24 02:12:10 +0700

    feat: config alias next

--- next.config.js
+++ next.config.js
@@ -1,6 +1,15 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true
-}
+const path = require("path");
 
-module.exports = nextConfig
+const nextConfig = {
+  reactStrictMode: true,
+  sassOptions: {
+    includePaths: [path.join(__dirname, "./src/styles")],
+    prependData: `@import "index.scss";`
+  },
+  images: {
+    domains: [process.env.NEXT_PUBLIC_DOMAIN_URL || "localhost", "127.0.0.1"]
+  }
+};
+
+module.exports = nextConfig;
