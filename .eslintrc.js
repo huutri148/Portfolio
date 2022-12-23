2d4b18db20d8eedf9a327c0da8a76639b8e9a8ed
huutri148 <huutri148@gmail.com>
2022-12-24 00:58:44 +0700

    feat: ininital app

--- .eslintrc.js
+++ .eslintrc.js
@@ -11,10 +11,47 @@ module.exports = {
     tsconfigRootDir: __dirname,
     project: ["./tsconfig.json"]
   },
-  plugins: ["react"],
+  plugins: ["@typescript-eslint", "react", "react-hooks"],
   rules: {
+    // TypeScript
+    "@typescript-eslint/consistent-type-imports": "error",
+    "@typescript-eslint/explicit-function-return-type": "off",
+    "@typescript-eslint/no-confusing-void-expression": [
+      "error",
+      { ignoreArrowShorthand: true, ignoreVoidOperator: true }
+    ],
+    "@typescript-eslint/no-unused-expressions": "error",
+    "@typescript-eslint/no-unused-vars": "off",
+
+    // React
+    "react/prefer-stateless-function": "warn",
     "react/react-in-jsx-scope": "off",
-    "react/jsx-uses-react": "off",
-    "react/jsx-filename-extension": [1, { extensions: [".ts", ".tsx"] }]
+    "react/function-component-definition": [
+      2,
+      {
+        namedComponents: "arrow-function",
+        unnamedComponents: "arrow-function"
+      }
+    ],
+    "react/jsx-filename-extension": ["error", { extensions: [".ts", ".tsx"] }],
+    "react/jsx-props-no-spreading": "off",
+    "react/require-default-props": 0,
+    "arrow-body-style": ["error", "as-needed"],
+    "react/self-closing-comp": [
+      "error",
+      {
+        component: true,
+        html: true
+      }
+    ],
+    "react-hooks/exhaustive-deps": "error",
+
+    // File styles
+    "linebreak-style": 0
+  },
+  settings: {
+    react: {
+      version: "detect"
+    }
   }
-}
+};
